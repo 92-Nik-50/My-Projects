2252af07f98fe7205552c2d6e896819d4b5c8499
92-Nik-50 <nik.korchomnyy@mail.ru>
2021-01-05 14:28:39 +0300

    main: layout

---
+++ todolist/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app" class="app">
+    <h1>TodoList</h1>
+    <Body/>
+  </div>
+</template>
+
+<script>
+import Body from './components/Body.vue'
+export default {
+  components: { Body },
+  name: 'App',
+}
+</script>
+
+<style lang="scss">
+#app {
+  justify-content: center;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  padding: 0.625rem;
+
+
+}
+.app{
+  max-width: 31.25rem;
+  max-height: 33.125rem;
+  margin: 0 auto ;
+  padding: 0.625rem;
+  box-shadow: 0px 5px 10px 3px #cccccc;
+  margin-top: 3.75rem;
+  font-size: 1.5rem;
+}
+h1{
+  letter-spacing: 0.25rem;
+  text-align: center;
+  color: #cd6829;
+  opacity: 0.6;
+  font-size: 2.5rem;
+}
+</style>
---
+++ todolist/src/components/Body.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <input type="text" class="input">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Body'
+}
+</script>
+
+<style lang="scss">
+  .container{
+  box-shadow: 0px 5px 10px 3px #cccccc;
+  padding: 0.625rem;
+  max-height: 31.25rem;
+  }
+  .input{
+  width: 99%;
+  font-size: 1.125rem;
+  }
+</style>
