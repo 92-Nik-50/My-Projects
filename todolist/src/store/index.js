e880d7064d4102e949e65530516d61a5cefbabb1
92-Nik-50 <nik.korchomnyy@mail.ru>
2021-01-12 13:35:41 +0300

    Toco-1: layout, deletion, addition

--- todolist/src/store/index.js
+++ todolist/src/store/index.js
@@ -3,4 +3,31 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({ })
+export default new Vuex.Store({
+    actions: {},
+    state: {
+        todos:[]
+    },
+    mutations: {
+        addTodos(state, text){
+            if(text == ""){
+                return
+            }
+            state.todos.push({
+                isCompete: false,
+                text: text
+            })
+        },
+        deleteTodos(state, index){
+            state.todos.splice(index, 1)
+        }
+    },
+    getters: {
+        listTodos(state){
+            return state.todos
+        },
+        totalTodos(state){
+            return state.todos.length
+        }
+    }
+ })
